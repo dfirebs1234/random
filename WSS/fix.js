a7f4e2de5dc6ed197554fa6c8709d9f84d30d044
Firebs1234 <firebs1234@gmail.com>
2013-04-15 20:28:37 -0400

    Second fix attempt

---
+++ WSS/fix.js
@@ -0,0 +1,4 @@
+var elem = document.getElementById("main");
+elem.setAttribute("style","box-shadow: 0px 0px 0px white !important;");
+elem = document.getElementByClass("banner");
+elem.setAttribute("style","display.none;");
