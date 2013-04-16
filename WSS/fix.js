f94ab1e6e234d09ee42289a0db1ebd1cc78af937
Firebs1234 <firebs1234@gmail.com>
2013-04-15 20:34:57 -0400

    Fix to remove banner and footer
    Fixes bookmarklet, wrong URL.

--- WSS/fix.js
+++ WSS/fix.js
@@ -1,4 +1,3 @@
-var elem = document.getElementById("main");
-elem.setAttribute("style","box-shadow: 0px 0px 0px white !important;");
-elem = document.getElementByClass("banner");
-elem.setAttribute("style","display.none;");
+document.getElementById("main").setAttribute("style","box-shadow: 0px 0px 0px white !important;");
+document.getElementByClassName("banner").setAttribute("style","display.none;");
+document.getElementById("foot").setAttribute("style","display:none;");
