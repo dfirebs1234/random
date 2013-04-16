603d1204c20f03fbe469589973621a46949cc2f9
Firebs1234 <firebs1234@gmail.com>
2013-04-15 20:48:37 -0400

    Last fix
    Really hope this works. Added redundancy to removing footer.

--- WSS/fix.js
+++ WSS/fix.js
@@ -1,3 +1,4 @@
 document.getElementById("main").setAttribute("style","box-shadow: 0px 0px 0px white !important;");
-document.getElementByClassName("banner").setAttribute("style","display: none;");
 document.getElementById("foot").setAttribute("style","display: none;");
+document.getElementById("foot").setAttribute("style","display: none;");	// In case it didn't work the first time...
+document.getElementsByClassName("banner")[0].setAttribute("style","display: none;");
