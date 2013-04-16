3d47ce97ffcd4e3dc1ab3bc24341fd4b516c0b55
Firebs1234 <firebs1234@gmail.com>
2013-04-15 20:43:15 -0400

    Fixed again
    Hopefully fixed it... hope, right?

--- WSS/fix.js
+++ WSS/fix.js
@@ -1,3 +1,3 @@
 document.getElementById("main").setAttribute("style","box-shadow: 0px 0px 0px white !important;");
-document.getElementByClassName("banner").setAttribute("style","display.none;");
-document.getElementById("foot").setAttribute("style","display:none;");
+document.getElementByClassName("banner").setAttribute("style","display: none;");
+document.getElementById("foot").setAttribute("style","display: none;");
